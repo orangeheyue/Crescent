2acd9217d415349f7eb591bee21726ffee9ed5e4
orangeai-research <orangeai.team@outlook.com>
2026-05-23 15:04:12 +0800

    add main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Description: main file
+# Author: orange
+# Date: 20260523
+
+import os
+import argparse
+from utils.quick_start import quick_start
+os.environ['NUMEXPR_MAX_THREADS'] = '48'
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', '-m', type=str, default='MGCN', help='model name')
+    parser.add_argument('--dataset', '-d', type=str, default='baby', help='dataset name')
+    parser.add_argument('--mg', action='store_true', help='whether to use Mirror Gradient, default is False')
+
+    config_dict = {
+        'gpu_id' : 0,
+    }
+
+    args, _ = parser.parse_known_args()
+
+    quick_start(
+        model=args.model,
+        dataset=args.dataset,
+        config_dict=config_dict,
+        save_model=True,
+        mg=args.mg
+    )
+
+
+
+
